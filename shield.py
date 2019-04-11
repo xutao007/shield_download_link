dbdcf599a56dc3b54aa5b3ce00046ad5e83f7688
xutao007 <xutao123454321@outlook.com>
2019-04-11 19:49:54 +0800

    change the resolution

--- shield.py
+++ shield.py
@@ -4,19 +4,38 @@ import xml.dom.minidom
 dom = parse("30675.xml")
 items = dom.documentElement.getElementsByTagName("channel")[0].getElementsByTagName("item")
 
+def get_download_link(item):
+    download_link = ""
+    ed2k = item.getElementsByTagName("ed2k")
+    magnet = item.getElementsByTagName("magnet")
+    pan = item.getElementsByTagName('pan')
+    if ed2k:
+        download_link = ed2k[0].firstChild.data
+    elif magnet:
+        download_link = magnet[0].firstChild.data
+    elif pan:
+        download_link = pan[0].firstChild.data
+    return download_link
+
+
 with open('ed2k.txt', 'w') as f:
     for item in items:
-
         title = item.getElementsByTagName("title")[0].firstChild.data
-        if title.endswith('.mp4') and title.startswith("神盾局特工"):
+        if "S01" in title or "S02" in title:
+            if title.endswith('.mkv') and title.startswith("神盾局特工"):
+                download_link = get_download_link(item)
+                f.write(download_link + '\n')    
 
-            download_link = ""
+        elif "S03" in title:
+            if "1024X576" in title and title.startswith("神盾局特工"):
+                download_link = get_download_link(item)
+                f.write(download_link + '\n')            
+        elif "S04" in title or "S05" in title:
+            if title.endswith('.mp4') and title.startswith("神盾局特工"):
+                download_link = get_download_link(item)
+                f.write(download_link + '\n')    
+        else:
+            if title.startswith("神盾局特工"):
+                download_link = get_download_link(item)
+                f.write(download_link + '\n')
 
-            ed2k = item.getElementsByTagName("ed2k")[0]
-            if ed2k:
-                download_link = ed2k.firstChild.data
-            else:
-                magnet = item.getElementsByTagName("magnet")[0]
-                download_link = magnet.firstChild.data
-
-            f.write(download_link + '\n')
